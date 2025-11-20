41b4d00b82535332834b6c003769dd1985eb543e
wellerson pinheiros <wellersonpinheiros@outlook.com>
2025-11-20 15:11:51 -0300

    :sparkles: Creat Currency Converter/ Project Java

---
+++ src/main/java/aplication/aplication.java
@@ -0,0 +1,11 @@
+package aplication;
+
+public class aplication {
+
+	public static void main(String[] args) {
+		String apikey = System.getenv("api-conversor-key_exchange_rate_api");
+		System.out.println("Minha chave: " + apikey);
+
+	}
+
+}
